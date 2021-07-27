e28cafca142eef30ff8c1db6f9b9c043cb9f40fb
Carlos V <cavarod@gmail.com>
2021-07-27 13:13:29 -0700

    finished 20.4.6

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
+	const [errorMessage, setErrorMessage] = useState('');
 	const [formState, setFormState] = useState({
 		name: '',
 		email: '',
@@ -8,26 +10,55 @@ function ContactForm() {
 	});
 
 	function handleChange(e) {
-		setFormState({...formState, name: e.target.value })
+
+		if(e.target.name === 'email') {
+			const isValid = validateEmail(e.target.value);
+			console.log(isValid);
+			
+			if(!isValid) {
+				setErrorMessage('Your email is invalid');
+			} else {
+				setErrorMessage('');
+			}
+		} else {
+			if (!e.target.value.length) {
+				setErrorMessage(`${e.target.name} is required.`);
+			} else {
+				setErrorMessage('');
+			}
+		}
+
+		if(!errorMessage) {
+			setFormState({...formState, [e.target.name]: e.target.value });
+		}
 	}
 
-	console.log(formState);
+	function handleSubmit(e) {
+		e.preventDefault();
+		console.log(formState);
+	}
+
 
 	return (
     	<section>
       		<h1>Contact me</h1>
-      		<form id="contact-form">
+      		<form id="contact-form" onSubmit={handleSubmit}>
     			<div>
   					<label htmlFor="name">Name:</label>
-  					<input type="text" name="name" defaultValue={formState.name} onChange={handleChange} />
+  					<input type="text" name="name" defaultValue={formState.name} onBlur={handleChange} />
 				</div>
 				<div>
   					<label htmlFor="email">Email address:</label>
-  					<input type="email" name="email" defaultValue={formState.name} />
+  					<input type="email" name="email" defaultValue={formState.name} onBlur={handleChange} />
 				</div>
 				<div>
   					<label htmlFor="message">Message:</label>
-  					<textarea name="message" rows="5" defaultValue={formState.name} />
+  					<textarea name="message" rows="5" defaultValue={formState.name} onBlur={handleChange} />
+  					{errorMessage && (
+  						<div>
+  							<p className="error-text">{errorMessage}</p>
+  						</div>
+  					)}
 				</div>
 				<button type="submit">Submit</button>
       		</form>
